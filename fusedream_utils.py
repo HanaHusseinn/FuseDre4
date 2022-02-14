870f9d6c537629ab5b1dc6021a3edd58b9ed398a
HanaHusseinn <53904283+HanaHusseinn@users.noreply.github.com>
2022-02-14 17:51:35 +0000

    Update fusedream_utils.py

--- fusedream_utils.py
+++ fusedream_utils.py
@@ -535,12 +535,14 @@ class FuseDreamBaseGenerator():
                             if (iter==0):
                                 y_init[num_basis-1-y_init_counter]=random.choice(nounsArrImageNet[0:endIndexNoun[iter]])
                                 y_init_counter=y_init_counter+1
-                            else:
+                            #case2: not first element, and elemnts before it is normal different number(the current noun has classes in ImageNet)
+                            #or: not first element, and elements before it is the same number(the current noun doesn't have class in ImageNet)
+                            elif (iter!=0 and endIndexNoun[iter-1]!=-1):
                                 y_init[num_basis+y_init_counter]=random.choice(nounsArrImageNet[endIndexNoun[iter-1]:endIndexNoun[iter]])
                                 y_init_counter=y_init_counter+1
                                 
-                            #case2: noun doesn't exist in ImageNet in the beginning so -1, so begin at 0
-                            if (iter!=0 and endIndexNoun[iter-1]==-1):
+                            #case3: not the first element, and the noun doesn't exist in ImageNet in the beginning so -1, so begin at 0
+                            elif (iter!=0 and endIndexNoun[iter-1]==-1):
                                 y_init[num_basis+y_init_counter]=random.choice(nounsArrImageNet[0:endIndexNoun[iter]])
                                 y_init_counter=y_init_counter+1
 
